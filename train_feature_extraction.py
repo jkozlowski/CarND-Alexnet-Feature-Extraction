5bca98fdd3602e2009b09ebf4110b26f301680ae
Jakub Kozlowski <mail@jakub-kozlowski.com>
2017-07-05 21:08:37 +0100

    Solution to lab

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -2,24 +2,119 @@ import pickle
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
+from sklearn.utils import shuffle
 
 # TODO: Load traffic signs data.
+training_file = "traffic-signs-data/train.p"
+validation_file= "traffic-signs-data/valid.p"
+testing_file = "traffic-signs-data/test.p"
 
-# TODO: Split data into training and validation sets.
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+    
+X_train, y_train = train['features'], train['labels']
+X_validation, y_validation = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+nb_classes = 43
 
 # TODO: Define placeholders and resize operation.
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+resized = tf.image.resize_images(x, [227, 227])
+#normalized = tf.image.per_image_standardization(resized)
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
+fc7 = AlexNet(resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
+shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
+fc8W = tf.Variable(tf.truncated_normal(shape=shape, mean = 0, stddev = 0.1))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+#logits = tf.nn.softmax(tf.nn.xw_plus_b(fc7, fc8W, fc8b))
+logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
 
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+y = tf.placeholder(tf.int32, (None))
+learning_rate = tf.placeholder(tf.float32)
+keep_prob = tf.placeholder(tf.float32)
+one_hot_y = tf.one_hot(y, nb_classes)
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8W, fc8b])
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+EPOCHS = 20
+BATCH_SIZE = 150
+BASE_RATE = 0.001
+DROPOUT_RATE = 0.5
+
+saver = tf.train.Saver()
 
 # TODO: Train and evaluate the feature extraction model.
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+def train_model(X_train, y_train, X_validation, y_validation):
+    current_rate = BASE_RATE
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        num_examples = len(X_train)
+
+        print("Training...")
+        print()
+        for i in range(EPOCHS):
+            if (i+1)%5==0:
+                current_rate=current_rate*0.8 
+            X_train_set, y_train_set = shuffle(X_train, y_train)
+            for offset in range(0, num_examples, BATCH_SIZE):
+                end = offset + BATCH_SIZE
+                batch_x, batch_y = X_train_set[offset:end], y_train_set[offset:end]
+                sess.run(training_operation, feed_dict={
+                    x: batch_x, 
+                    y: batch_y, 
+                    learning_rate: current_rate, 
+                    keep_prob: DROPOUT_RATE})
+
+            training_accuracy = evaluate(X_train, y_train)
+            validation_accuracy = evaluate(X_validation, y_validation)
+            print("EPOCH {}, rate {} ...".format(i+1, current_rate))
+            print("Training Accuracy = {:.3f}".format(training_accuracy))
+            print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+            print()
+
+        saver.save(sess, './traffic_sign.backup')
+        print("Model saved")
+        
+def test_model(X_test, y_test):
+    with tf.Session() as sess:
+        
+        print("Testing...")
+        print()
+        
+        saver.restore(sess, './traffic_sign.backup')
+
+        test_accuracy = evaluate(X_test, y_test)
+        print("Test Accuracy = {:.3f}".format(test_accuracy))
+        
+train_model(X_train, y_train, X_validation, y_validation)
+test_model(X_test, y_test)
